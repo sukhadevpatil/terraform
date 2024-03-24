2bba262793156d058affb8ee99acbbf8b36bad68
Sukhdev Patil <sukhadev.patil@gmail.com>
2024-03-24 22:15:45 +0530

    first commit

--- Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
+++ Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket = "kplabs-remote-backend"
     key    = "ec2demo.tfstate"
     region = "us-west-1"
-    access_key = "access_key"
-    secret_key = "secret_key"
+    /*access_key = "access_key"
+    secret_key = "secret_key"*/
     dynamodb_table = "s3-state-lock"
   }
 }
